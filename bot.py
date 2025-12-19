619d091b2218383f4087a41ba89baa1ba4d167bd
rapidex17 <intima.compaixao@gmail.com>
2025-12-18 22:07:20 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🤖 Bot online com sucesso!\n\nUse /start para começar."
+    )
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot iniciado...")
+    app.run_polling()
+
+if _name_ == "_main_":
+    main()
